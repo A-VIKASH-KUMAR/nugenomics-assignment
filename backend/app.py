435d182d6a9a3796b72467a89295b981c6d1dacf
A-Vikash <vickyanga@gmail.com>
2024-08-08 23:33:37 +0530

    feat: ✨ add frontend login and register page

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
 from auth.auth import auth_router
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow requests from all origins
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(auth_router)
+
 
 @app.get("/")
 async def root():
